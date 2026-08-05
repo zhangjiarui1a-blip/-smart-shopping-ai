914ab3804682031e64b197d195d1cdef6465531a
zhangjiarui1a <zhangjiarui1a@gmail.com>
2026-08-05 11:29:41 +0800

    fix: configure CloudBase recommendation endpoint

---
+++ runtime-config.js
@@ -0,0 +1,3 @@
+// 填写 CloudBase recommendations HTTP 云函数的完整公开访问地址，例如：
+// window.RECOMMENDATIONS_API_URL = 'https://xxxxxxxx.service.tcloudbase.com/recommendations';
+window.RECOMMENDATIONS_API_URL = '';
